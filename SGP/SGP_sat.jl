3478f5a55d222790eb84a20b95a028aa2674e3bf
ArthurG <arthur.pro.gontier@gmail.com>
2019-10-09 18:21:15 +0200

    ctr1 + écriture dans fichier

--- SGP/SGP_sat.jl
+++ SGP/SGP_sat.jl
@@ -15,23 +15,46 @@ function pla(i,j,k)
 	return i + g*(j-1) + w*g*(k-1)
 end  
 
-cpt = 1
+function sat(w,g,p,q)
+	cpt = 1
+	open("testsat.txt","w") do f
+	# ctr 1
+	for i in 1:w
+		for k in 1:q
+			s = string(cpt)
+			cpt = cpt + 1
+			for j in 1:g
+				s = s*" "*string(pla(i,j,k))
+			end
+			s = s*" 0"
+			write(f,s*"\n")
+		end
+	end
+	for i in 1:w
+		for j in 1:g
+			for jj in 1:g
+				if jj != j
+					for k in 1:q
+						write(f,string(cpt," -",pla(i,j,k)," -",pla(i,jj,k)," 0\n"))
+					end
+				end
+			end
+		end
+	end
 
+	# ctr 2
 
-
-
-# ctr 3
-for k in 1:q
-	for kk in 1:q
-		if kk != k
-			for i in 1:w
-				for ii in 1:w
-					if ii != i
+	# ctr 3
+	for k in 1:q
+		for kk in 1:q
+			if kk != k
+				for i in 1:w
+					for ii in 1:w
 						for j in 1:g
 							for jj in 1:g
-								if jj != j
-									println(cpt," -",pla(i,j,k)," -",pla(i,j,kk)," -",pla(ii,jj,k)," -",pla(ii,jj,kk)," 0")
-									global cpt = cpt + 1
+								if jj != j || ii!=j
+									write(f,string(cpt," -",pla(i,j,k)," -",pla(i,j,kk)," -",pla(ii,jj,k)," -",pla(ii,jj,kk)," 0\n"))
+									cpt = cpt + 1
 								end
 							end
 						end
@@ -40,6 +63,7 @@ for k in 1:q
 			end
 		end
 	end
+	end
 end
 
-
+sat(w,g,p,q)
