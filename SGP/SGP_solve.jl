b418d0bf11863941ab30147cebca2ee696ad8891
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-10-09 16:51:44 +0200

    ajout des premières fonctions

---
+++ SGP/SGP_solve.jl
@@ -0,0 +1,90 @@
+
+# Une variable contient deux ensembles, et deux entiers
+mutable struct Variable
+    min::Set{Int64}
+    max::Set{Int64}
+    card_min::Int64
+    card_max::Int64
+    est_clot::Bool
+end
+
+function generer_Variable(min::Set{Int64}, max::Set{Int64},
+    card_min::Int64, card_max::Int64, est_clot::Bool = false)
+    return Variable(Set{Int64}(min), Set{Int64}(max), card_min, card_max, est_clot)
+end
+
+# On modifie le print lors d'un print(var::Variable) ou d'un println(var::Variable)
+function Base.show(io::IO, var::Variable)
+    println("Variable :")
+    println("\tmin : ", var.min)
+    println("\tmax : ", var.max)
+    println("\t", var.card_min, " <= Cardinal <= ", var.card_max)
+end
+
+# Vérifie si la variable reste valide, ie qu'il reste des solutions possibles.
+function verifie_validite(var::Variable)
+    valide = true
+    valide = valide && intersect(var.min, var.max) == var.min
+    valide = valide && length(var.max) >= var.card_min
+    valide = valide && length(var.min) <= var.card_max
+    valide = valide && var.card_min <= var.card_max
+    return valide
+end
+
+# Vérifie si la variable est close.
+function verifie_clot(var::Variable)
+    clot = false
+    clot = clot || var.min == var.max
+    clot = clot || length(var.min) == var.card_max
+    var.est_clot = clot
+    return clot
+end
+
+# on filtre la contrainte : (var1 inter var2) = emptyset
+function filtrage_intersection_vide(var1::Variable, var2::Variable, Univers::Set)
+    if (!var1.est_clot)
+        setdiff!(var1.max, var2.min)
+        var1.card_max = max(var1.card_max, length(setdiff( Univers, var2.min )) )
+    end
+    if (!var2.est_clot)
+        setdiff!(var2.max, var1.min)
+        var2.card_max = max(var2.card_max, length(setdiff( Univers, var1.min )) )
+    end
+end
+
+function filtrage_card_intersection(var1::Variable, var2::Variable)
+    # TODO :
+end
+
+
+#==============================================================================#
+#============================= Test des fonctions =============================#
+#==============================================================================#
+
+Univers = Set((1, 2, 3, 4, 5, 6))
+v1 = generer_Variable(Set{Int64}((1, 2, 3)), Univers, 2, 4)
+v2 = generer_Variable(Set{Int64}((5, 6)), Univers, 2, 4)
+v3 = generer_Variable(Set{Int64}((3, 5, 6)), Univers, 2, 4)
+function test_validite(var::Variable, varName::String = "var")
+    println(varName, " ", verifie_validite(var) ? "est valide." : "n'est pas valide.")
+end
+
+function test_clot(var::Variable, varName::String = "var")
+    println(varName, " ", verifie_clot(var) ? "est clot." : "n'est pas clot.")
+end
+
+function test_filtrage_intersection_vide(var1::Variable, var2::Variable, Univers)
+    println("---------------------------------------")
+    println("Test de la contrainte intersection vide")
+    println("---------------------------------------")
+    print("var1 : ", var1, "var2 : ", var2)
+    println("Filtrage (var1 inter var2) = emptySet.")
+    filtrage_intersection_vide(var1, var2, Univers)
+    print("var1 : ", var1, "var2 : ", var2)
+    test_validite(var1, "var1")
+    test_validite(var2, "var2")
+    test_clot(var1, "var1")
+    test_clot(var2, "var2")
+end
+test_filtrage_intersection_vide(v1, v2, Univers)
+test_filtrage_intersection_vide(v1, v3, Univers)
