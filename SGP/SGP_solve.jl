a967492936732a5004eb980f7d5ac7aaacb06b8c
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-11-06 16:25:23 +0100

    Ajout de l'id et essaie de changement de la fonction show

--- SGP/SGP_solve.jl
+++ SGP/SGP_solve.jl
@@ -10,20 +10,41 @@ mutable struct Variable
     card_min::Int64
     card_max::Int64
     est_clot::Bool
+
+    id::String
+
+    Variable(min::Set{Int64}, max::Set{Int64},
+            card_min::Int64, card_max::Int64,
+            est_clot::Bool = false) = new(
+        Set{Int64}(min),
+        Set{Int64}(max),
+        card_min,
+        card_max,
+        est_clot,
+        generer_random_id(20)
+    )
+end
+
+function generer_random_id(taille::Int)
+    STR_RANDOM_ID_SEED = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?.!+-*/^%&"
+    str = ""
+    for i in 1:taille
+        str *= rand(STR_RANDOM_ID_SEED)
+    end
+    return str
+end
+
+function egal_id(var1::Variable, var2::Variable)
+    return var1.id == var2.id
 end
 
 # Fait l'intersection la plus large des variables
-function intersect(var::Variable, var::Variable)
+function intersection(var1::Variable, var2::Variable)
     min = intersect(var1.min, var2.min)
     max = intersect(var1.max, var2.max) # On ne peut pas trouver moins large
     card_min = length(min)
     max = length(max)
-    return generer_Variable(min, max, card_min, card_max)
-end
-
-function generer_Variable(min::Set{Int64}, max::Set{Int64},
-    card_min::Int64, card_max::Int64, est_clot::Bool = false)
-    return Variable(Set{Int64}(min), Set{Int64}(max), card_min, card_max, est_clot)
+    return Variable(min, max, card_min, card_max)
 end
 
 # Génère une variable vide
@@ -42,10 +63,30 @@ end
 
 # On modifie le print lors d'un print(var::Variable) ou d'un println(var::Variable)
 function Base.show(io::IO, var::Variable)
-    println("Variable :")
-    println("\tmin : ", var.min)
-    println("\tmax : ", var.max)
-    print("\t", var.card_min, " <= Cardinal <= ", var.card_max)
+    compact = get(io, :compact, false)
+    sep = ", "
+    if !compact
+        println(io, "Variable :")
+        println(io, "\tmin : ", var.min)
+        println(io, "\tmax : ", var.max)
+        println(io, "\t", var.card_min, " <= Cardinal <= ", var.card_max)
+        println(io, "\tid: ", var.id)
+    else
+        print(io, "Variable(")
+        print(io, "min:")
+        show(io, var.min)
+        print(io, sep)
+        print(io, "max:")
+        show(io, var.max)
+        print(io, sep)
+        show(io, var.card_min)
+        print(io, " <= Cardinal <= ")
+        show(io,  var.card_max)
+        print(sep)
+        print(io, "id:")
+        show(io, var.id)
+        print(io, ")")
+    end
 end
 
 # Vérifie si la variable reste valide, ie qu'il reste des solutions possibles.
@@ -99,18 +140,26 @@ end
 
 struct Contrainte
     liste_argument::Array{Variable, 1}
-    filtrage!::Fonction
+    filtrage!::Function
 end
 
-function solver_generique(liste_contrainte::Array{Contrainte, 1}, liste_variable::Array{Variable, 1}, Univers::Set)
+function filtrer(ctr::Contrainte, univers::Set)
+    ctr.filtrage!(liste_argument, univers)
+end
+
+function solver_generique(liste_contraintes::Array{Contrainte, 1}, liste_variables::Array{Variable, 1}, Univers::Set)
     # Dictionnaire qui associe toutes les contraintes d'une variable à une variable
-    dict_contrainte_variable = Dict{Int, Array{Contrainte, 1}}()
-    for i in length(liste_variable)
-        var = liste_variable[i]
-        # TODO
+    dict_contrainte_variable = Dict{String, Array{Int, 1}}()
+    for var in liste_variables
+        dict_contrainte_variable[var.id] = findall(ctr -> var in ctr.liste_argument, liste_contraintes)
     end
 
-    liste_filtrage_restant = copy(liste_contrainte)
+    liste_filtrage_restant = collect(length(liste_contraintes):-1:1)
+    while !isempty(liste_filtrage_restant)
+        index_ctr = pop!(liste_filtrage_restant)
+        ctr = liste_contraintes[index_ctr]
+    end
+
 
 end
 
@@ -120,9 +169,9 @@ end
 #==============================================================================#
 
 Univers = Set((1, 2, 3, 4, 5, 6))
-v1 = generer_Variable(Set{Int64}((1, 2, 3)), Univers, 2, 4)
-v2 = generer_Variable(Set{Int64}((5, 6)), Univers, 2, 4)
-v3 = generer_Variable(Set{Int64}((3, 5, 6)), Univers, 2, 4)
+v1 = Variable(Set{Int64}((1, 2, 3)), Univers, 2, 4)
+v2 = Variable(Set{Int64}((5, 6)), Univers, 2, 4)
+v3 = Variable(Set{Int64}((3, 5, 6)), Univers, 2, 4)
 
 function test_validite(var::Variable, varName::String = "var")
     println(varName, " ", verifie_validite(var) ? "est valide." : "n'est pas valide.")
