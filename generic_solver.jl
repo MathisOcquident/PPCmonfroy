9a3990c28563c83ed0713803efb66c0a5fb5dd83
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-11-22 13:33:03 +0100

    Changement de inferieur

--- generic_solver.jl
+++ generic_solver.jl
@@ -186,7 +186,8 @@ function branch_and_bound!(liste_variables::Array{Variable, 1}, liste_contrainte
     function inf_diff_card(left, right)
         left_var = liste_variables[left]
         right_var = liste_variables[right]
-        return left_var.card_max - left_var.card_min < right_var.card_max - right_var.card_min
+        return min(left_var.card_max, length(left_var.max)) - length(left_var.min) <=
+            min(right_var.card_max, length(right_var.max)) - length(right_var.min)
     end
     faisable = solver_generique!(liste_variables, liste_contraintes)
     if faisable
