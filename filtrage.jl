95af773a692969e0f59d962c8d7f5dadafdc4284
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-12-17 22:44:03 +0100

    Suppression de quelques trucs inutile

--- filtrage.jl
+++ filtrage.jl
@@ -2,14 +2,11 @@
 function filtrage_intersection_vide!(liste_Variable::Array{Variable, 1})
     var1, var2 = liste_Variable
     setdiff!(var1.max, var2.min)
-    var1.card_max = min(var1.card_max, length(setdiff( var1.univers, var2.min )) )
-
     setdiff!(var2.max, var1.min)
-    var2.card_max = min(var2.card_max, length(setdiff( var2.univers, var1.min )) )
-
     return nothing
 end
 
+# on filtre la contrainte : |(var1 inter var2)| <= 1
 function filtrage_card_intersection_inferieur_1!(liste_Variable::Array{Variable, 1})
     var1, var2 = liste_Variable
     inter = intersect(var1.min, var2.min)
@@ -27,15 +24,6 @@ function filtrage_card_intersection_inferieur_1!(liste_Variable::Array{Variable,
         filtrage_individuel!(var1)
         filtrage_individuel!(var2)
     end
-
-    omega = union(var1.max, var2.max)
-    n = length(omega)
-    n1 = length(var1.min)
-    n2 = length(var2.min)
-
-    var1.card_max = min(var1.card_max, n+1 - n2 )
-    var2.card_max = min(var2.card_max, n+1 - n1 )
-
     return nothing
 end
 
@@ -45,13 +33,8 @@ function filtrage_triple_intersection_vide!(liste_Variable::Array{Variable, 1})
     var1, var2, var3 = liste_Variable
 
     setdiff!(var1.max, intersect(var2.min, var3.min))
-    var1.card_max = min(var1.card_max, length(setdiff( var1.univers, intersect(var2.min, var3.min) )) )
-
     setdiff!(var2.max, intersect(var1.min, var3.min))
-    var2.card_max = min(var2.card_max, length(setdiff( var2.univers, intersect(var1.min, var3.min) )) )
-
     setdiff!(var3.max, intersect(var2.min, var1.min))
-    var3.card_max = min(var3.card_max, length(setdiff( var3.univers, intersect(var2.min, var1.min) )) )
 
     return nothing
 end
