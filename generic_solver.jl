e751a86585e3c7ecda227ceafaa9961022b1b541
Fontaine Jonathan <fontaine.jonathan44@gmail.com>
2019-12-18 10:00:44 +0100

    Correction d'une erreur dans le code qui n'apparaisse pas dans nos cas

--- generic_solver.jl
+++ generic_solver.jl
@@ -225,6 +225,7 @@ function branch_and_bound!(liste_variables::Array{Variable, 1}, liste_contrainte
                     liste_variables[i] = liste_variables_temp[i]
                 end
             else
+                faisable_temp = branch_and_bound!(liste_variables, liste_contraintes)
                 faisable = faisable_temp # pas faisable
             end
         end
